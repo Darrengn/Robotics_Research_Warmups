faace47e15a8ad4ff2ada5652cfc414d07ab1790
DarrenNg <ngwdarren@gmail.com>
2024-07-10 11:41:26 -0700

    added code for moving to pose

--- Week2/pybullet_test.py
+++ Week2/pybullet_test.py
@@ -10,21 +10,22 @@ def useKeyboard():
     inputs = p.getKeyboardEvents()
     if ord('q') in inputs:
         followLine(1,1,3)
-    
+    if ord('r') in inputs:
+        moveToPose(3,3,np.pi/2)
     if p.B3G_RETURN in inputs:
-        moveToPoint(-2,0)
+        moveToPoint(3,3)
     if p.B3G_DELETE in inputs:
         getCenterDepth()
     if p.B3G_LEFT_ARROW in inputs:
-        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[-20,-20,20,20],forces = [5]*4)
+        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[-20,-20,20,20],forces = [3]*4)
     elif p.B3G_RIGHT_ARROW in inputs:
-        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[20,20,-20,-20],forces = [5]*4)
+        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[20,20,-20,-20],forces = [3]*4)
     elif p.B3G_UP_ARROW in inputs:
-        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[-100,-100,-100,-100],forces = [5]*4)
+        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[-100,-100,-100,-100],forces = [3]*4)
     elif p.B3G_DOWN_ARROW in inputs:
-        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[20,20,20,20],forces = [5]*4)
+        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[20,20,20,20],forces = [3]*4)
     else:
-        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[0,0,0,0],forces = [5]*4)
+        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[0,0,0,0],forces = [3]*4)
 
 
 pmat = p.computeProjectionMatrixFOV(fov=120, aspect=(128 / 128), nearVal=0.01, farVal=20)
@@ -65,10 +66,10 @@ def calcAngleErr(tar, theta):
 def moveToPoint(xtar, ytar):
     erraccum_lin = 0
     erraccum_ang = 0
-    kp_l = 50 #50
-    ki_l = 0.00
+    kp_l = 100 #50
+    ki_l = 0
     kd_l = 0.0
-    kp_a = 1000
+    kp_a = 2000
     ki_a = 0.0
     kd_a = 0
 
@@ -134,12 +135,91 @@ def moveToPoint(xtar, ytar):
     p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=4*[0],forces = [5]*4)
     print("DONE, Curpos:", curpos)
 
+def moveToPose(xtar, ytar, end_ang):
+    kp = 25
+    ka = 0.9
+    kb = -0.1      #-0.05
+    kp_theta = 1000
+    
+    curpos, curorn = p.getBasePositionAndOrientation(robot)
+    rot_matrix = p.getMatrixFromQuaternion(curorn)
+    rot_matrix = np.array(rot_matrix).reshape(3, 3)
+    y_head = rot_matrix[:,1]
+    theta = np.arccos(y_head.dot([1,0,0]))
+
+    if y_head[1] < 0:
+        theta = -theta
+    x,y,z = curpos
+
+    ang_to_goal = np.arctan2((ytar-y),(xtar-x))
+
+    err = ((ytar - y)**2 + (xtar - x)**2)**0.5
+    alpha = calcAngleErr(ang_to_goal,theta)
+    beta = calcAngleErr(-theta, alpha)
+    beta = calcAngleErr(beta, -end_ang)
+
+    # beta = end_ang
+    k_vec = np.array([ka/err, kb*err])
+    k_vec = k_vec/np.linalg.norm(k_vec)
+
+
+    theta_tar = k_vec[0]*alpha + k_vec[1]*beta
+    # theta_tar = calcAngleErr(theta_tar, -np.pi/2)
+    # err_ang = calcAngleErr(theta_tar,theta)
+    err_ang = calcAngleErr(end_ang,theta)
+    
+    while abs(err) > 0.1 or abs(err_ang) > 0.03:
+        # print("Error:",err_ang, " Acum Err:",erraccum_ang, " D Err:", derr_ang)
+        
+        v = -kp*err
+        w = -kp_theta*theta_tar*abs(theta_tar)
+        # print("Left Vel:", v-0.22*w, "  Right Vel:", v+0.22*w)
+        # print("tar vel: ", v, " tar angvel", w)
+        p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[v+0.22*w,v+0.22*w,v-0.22*w,v-0.22*w],forces = [3]*4)
+        p.stepSimulation()
+        time.sleep(1./240.)
+
+        curpos, curorn = p.getBasePositionAndOrientation(robot)
+        rot_matrix = p.getMatrixFromQuaternion(curorn)
+        rot_matrix = np.array(rot_matrix).reshape(3, 3)
+        y_head = rot_matrix[:,1]
+        theta = np.arccos(y_head.dot([1,0,0]))
+
+        if y_head[1] < 0:
+            theta = -theta
+        x,y,z = curpos
+
+        ang_to_goal = np.arctan2((ytar-y),(xtar-x))
+
+        err = ((ytar - y)**2 + (xtar - x)**2)**0.5
+        alpha = calcAngleErr(ang_to_goal,theta)
+        beta = calcAngleErr(-theta, alpha)
+        beta = calcAngleErr(beta, -end_ang)
+
+        k_vec = np.array([ka/err, kb*err])
+        k_vec = k_vec/np.linalg.norm(k_vec)
+
+        if abs(err) < 0.05:
+            k_vec = [0, 1]
+        theta_tar = k_vec[0]*alpha + k_vec[1]*beta
+        # theta_tar = calcAngleErr(theta_tar, -np.pi/2)
+
+        err_ang = calcAngleErr(end_ang,theta)
+        
+        print("theta tar:",np.rad2deg(theta_tar), "  alpha:", np.rad2deg(alpha), "  beta", np.rad2deg(beta))
+        
+        cameraDisp()
+
+    p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=4*[0],forces = [5]*4)
+    print("DONE, Curpos:", curpos, "  Cur Angle:", np.rad2deg(theta))
+
 # where the line is ax + by + c = 0
 def followLine(a, b, c):
-    kd = 1000   #distance const
+    kd = 500   #distance const
     kh = 1000   #heading const
     while(True):
         inputs = p.getKeyboardEvents()
+        cameraDisp()
         if ord('e') in inputs:
             break
         curpos, curorn = p.getBasePositionAndOrientation(robot)
@@ -162,7 +242,7 @@ def followLine(a, b, c):
         w = gamma
         print(ang_err)
         
-        v = -40
+        v = -60
         p.setJointMotorControlArray(robot, [2,3,6,7], p.VELOCITY_CONTROL, targetVelocities=[v+0.22*w,v+0.22*w,v-0.22*w,v-0.22*w],forces = [3]*4)
         p.stepSimulation()
         time.sleep(1./240.)
